95ae3db6f91477b1a7745718bd5bca26f6b58106
unknown <tiucado@hotmail.com>
2023-02-17 09:26:46 -0300

    first commit

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,22 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <section className='rodape-page'>
+            <div className='plats'>
+                <img src='/imagens/fb.png'/>
+                <img src='/imagens/ig.png'/>
+                <img src='/imagens/tw.png'/>
+            </div>
+            <div>
+                <img src='/imagens/logo.png'/>
+            </div>
+            <div>
+                <h4> Desenvolvido por Ricardo.</h4>
+            </div>
+        </section>
+        
+    )
+}
+
+export default Rodape;
